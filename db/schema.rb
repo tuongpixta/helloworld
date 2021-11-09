8c30661c2124149bfee7c1c19d27a6896bb12e16
tuongpixta <tuong.nguyendinh@pixta.co.jp>
2021-11-09 10:32:14 +0700

    change db sqlite3 to mysql2

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_11_08_071107) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
